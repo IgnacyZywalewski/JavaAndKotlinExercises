1db5421bdf3e2aa4cace281f6c6f5ab45871e694
IgnacyZywalewski <127679558+IgnacyZywalewski@users.noreply.github.com>
2024-02-24 23:20:00 +0100

    Update Lista 2.kt

--- Kotlin/Lista 2.kt
+++ Kotlin/Lista 2.kt
@@ -8,31 +8,70 @@ fun missingNumber(array: IntArray): Int{
     }
 
     val set = array.toSet()
-    if(set.size != array.size){
-        println("Nie wszystkie elementy sa unikalne")
+    if (set.size != array.size) {
+        println("Nie wszystkie elementy są unikalne")
         return -1
     }
 
-    val n = array.size
-    var missingNumber = -1
-    array.sort()
 
-    for(num in 0..n-1){
-        if(array[num] != num){
-            missingNumber = num
-            break
+    val map = mutableMapOf<Int, Int>()
+    var missingNum : Int = 0
+
+    for(num in array){
+        if(map.containsKey(num)) { map[num] = map[num]!! +1 }
+        else { map[num] = 1 }
+    }
+
+    for(i in 0..array.size){
+        if(!map.containsKey(i)) { missingNum = i }
+    }
+
+    return missingNum
+}
+
+fun isPalindrome(arg : String): Boolean{
+    val n = arg.length
+
+    for(i in 0..n/2){
+        if(arg[i] != arg[n-1-i]){
+            println("\nPodane słowo nie jest palindromem")
+            return false
         }
     }
 
-    if(missingNumber == -1){
-        missingNumber = n
+    println("\nPodane słowo jest palindromem")
+    return true
+}
+
+fun printPascal(height: Int) {
+    require(height >= 1) { "Wysokość trójkąta Pascala musi być co najmniej 1" }
+
+    val triangle = mutableListOf<List<Int>>()
+
+    for (i in 0..< height) {
+        val row = mutableListOf<Int>()
+
+        for (j in 0..i) {
+            val value = if (j == 0 || j == i) 1 else triangle[i - 1][j - 1] + triangle[i - 1][j]
+            row.add(value)
+        }
+
+        triangle.add(row)
     }
 
-    println("Brakujaca liczba: ")
-    return missingNumber
+    println("\nTrójkąt Pascala wysokości $height:")
+    for (row in triangle) {
+        println(row.joinToString(" "))
+    }
 }
 
 fun main() {
-    val numbers = intArrayOf(2, 4, 5, 3, 0, 6)
+    val numbers = intArrayOf(2, 4, 5, 3, 1, 6)
     println(missingNumber(numbers))
+
+    val word: String = "abba"
+    println(isPalindrome(word))
+
+    val height: Int = 4
+    println(printPascal(height))
 }
