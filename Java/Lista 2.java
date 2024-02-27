793fa8deecaf5d8239893a0808f2ed6842f8a843
IgnacyZywalewski <127679558+IgnacyZywalewski@users.noreply.github.com>
2024-02-27 11:59:07 +0100

    Update Lista 2.java

--- Java/Lista 2.java
+++ Java/Lista 2.java
@@ -1,37 +1,71 @@
-import java.util.HashSet;
 import java.util.HashMap;
 
 public class Main {
     public static int missingNumber(int[] tab) {
         HashMap<Integer, Integer> map = new HashMap<>();
-        int missingNumber = -1;
 
         for (int num : tab) {
             if (num < 0) {
                 System.out.println("Występuje liczba ujemna");
-                break;
+                return -1;
             }
 
-            if (map.containsKey(num)) {
-                System.out.println("Występuje element powtarzający się");
-                return -1;
-            } else {
-                map.put(num, 1);
-            }
+            map.put(num, map.getOrDefault(num, 0) + 1);
         }
+
+        if(map.size() != tab.length){
+            System.out.println("Wysępuje element powtarzający się");
+            return -1;
+        }
+
+        int missingNumber = -1;
+        int flag = 0;
 
         for (int i = 0; i <= tab.length; i++) {
             if (!map.containsKey(i)) {
+                flag++;
                 missingNumber = i;
-                break;
             }
+        }
+
+        if(flag > 1){
+            System.out.println("Brakuje więcej niż jednego elementu");
+            return -1;
         }
 
         return missingNumber;
     }
+    public static Boolean isPalindrome(String word){
+        int n = word.length();
+        for(int i = 0; i < n/2; i++){
+            if(word.charAt(i) != word.charAt(n - 1 - i)){
+                return false;
+            }
+        }
+        return true;
+    }
+    public static void printPascal(Integer height){
+        for (int line = 1; line <= height; line++) {
+            for (int j = 0; j <= height - line; j++) {
+                System.out.print(" ");
+            }
+
+            int C = 1;
+
+            for (int i = 1; i <= line; i++) {
+                System.out.print(C + " ");
+                C = C * (line - i) / i;
+            }
+
+            System.out.println();
+        }
+    }
 
     public static void main(String[] args) {
-        int[] numbers = {0, 1, 2, 3};
+        int[] numbers = new int[]{0, 1, 2, 3, 4, 5};
+
         System.out.println(missingNumber(numbers));
+        System.out.println(isPalindrome("abba"));
+        printPascal(4);
     }
 }
