c1ddac8ebbc1a10ae0bcf9a4398e5f5ff2cca7f8
IgnacyZywalewski <127679558+IgnacyZywalewski@users.noreply.github.com>
2024-03-06 14:21:45 +0100

    Update Lista 1.java

--- Java/Lista 1.java
+++ Java/Lista 1.java
@@ -1,17 +1,23 @@
+import java.util.HashMap;
+import java.util.Map;
+
 public class Main {
     public static String checkDivisibility(int arg){
         StringBuilder ans = new StringBuilder();
         int[] numbers = {3, 5, 7, 11, 13, 15, 21};
+        Map<Integer, String> map = new HashMap<>();
+
+        map.put(3, "trzy");
+        map.put(5, "piec");
+        map.put(7, "siedem");
+        map.put(11, "jedenascie");
+        map.put(13, "trzynascie");
+        map.put(15, "pienascie");
+        map.put(21, "dwadziesciajeden");
 
         for (int number : numbers) {
             if (arg % number == 0) {
-                if (number == 3) ans.append("trzy");
-                if (number == 5) ans.append("piec");
-                if (number == 7) ans.append("siedem");
-                if (number == 11) ans.append("jedenascie");
-                if (number == 13) ans.append("trzynascie");
-                if (number == 15) ans.append("pienascie");
-                if (number == 21) ans.append("dwadziesciajeden");
+                ans.append(map.get(number));
             }
         }
         return ans.toString();
