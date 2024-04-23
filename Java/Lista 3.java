1e93a977b3a98767428e044b9a46f358f53ac116
IgnacyZywalewski <127679558+IgnacyZywalewski@users.noreply.github.com>
2024-04-23 23:19:27 +0200

    Update Lista 3.java

--- Java/Lista 3.java
+++ Java/Lista 3.java
@@ -1,19 +1,18 @@
 import java.util.*;
 
 public class Main {
-    public static Map<String, Integer> numberOfOccurrences(String word) {
+    public static void numberOfOccurrences(String word) {
         String[] array = word.split("[^\\p{L}]+");
         Map<String, Integer> map = new HashMap<>();
 
-        for (String x : array) {
-            map.put(x, map.getOrDefault(x, 0) + 1);
+        for (String s : array) {
+            map.put(s, map.getOrDefault(s, 0) + 1);
         }
 
         for (String w : map.keySet()) {
-            System.out.println("Word: " + w + " occurs " + map.get(w) + " times.");
+            System.out.println("Word: '" + w + "' occurs " + map.get(w) + " times.");
         }
 
-        return map;
     }
 
     public static Set<Integer> findDuplicates(int[] args) {
@@ -25,12 +24,11 @@ public class Main {
                 duplicates.add(num);
             }
         }
-
         return duplicates;
     }
 
     public static Map<Integer, Boolean> addToBoolean() {
-        HashMap<Integer, Boolean> map = new HashMap<Integer, Boolean>();
+        HashMap<Integer, Boolean> map = new HashMap<>();
 
         for(int i = 1; i <= 20; i++){
             if(i % 2 == 0){
@@ -43,12 +41,15 @@ public class Main {
     }
 
     public static void main(String[] args) {
+        //Zadanie 1
         String sentence = "To jest przykładowe zdanie. To zdanie jest tylko przykładowe.";
         numberOfOccurrences(sentence);
 
+        //Zadanie 2
         int[] list = {0, 1, 1, 1, 4, 4, 4, 9, 3, 3, 3, 3, 3, 3};
         System.out.println(findDuplicates(list));
 
+        //Zadanie 3
         System.out.println(addToBoolean());
     }
 }
